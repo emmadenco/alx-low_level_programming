fa0f5ac2712f73c353f2e2bd0c5067770c728977
Emmanuel <emmaadeyeye@gmail.com>
2022-04-07 16:52:57 -0700

    Task 4 fix6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,8 +8,10 @@
 
 /**
  * main function returns void
+ * and returns 0
+ * void returns 0
  */
-int main(void) 
+int main(void)
 {
 	 /*print function using puts*/
 		puts("\"Programming is like building a multilingual puzzle");
