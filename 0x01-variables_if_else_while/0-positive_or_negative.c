ff25c3d0bdebc7858a21fe6cce66fd380377ddf7
Emmanuel <emmaadeyeye@gmail.com>
2022-04-09 15:40:49 -0700

    Task 0 fix8

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,18 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
 /**
- *main(void) is the main funtion that embodies other funtion
- *and its type is int
- *and it contains no argument hence the void
- *the program assigns a random number to the variable n
- *each time it is executed
- *and it returns an integer then 0 when the task is completed
-*/
-int main(void) /*void always return zero no argument for voide*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
 {
 	int n;
 
