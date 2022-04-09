d5c01dcd8d3fb154bee0fe0b986b41bac966295f
Emmanuel <emmaadeyeye@gmail.com>
2022-04-09 14:58:38 -0700

    Task 0fix6

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,6 +6,9 @@
 /* betty style doc for function main goes there */
 /**
  *main void is the main funtion
+ *the program assigns a random number to the variable n
+ *each time it is executed
+ *and it returns an integer then 0 when the task is completed
 */
 int main(void)
 {
@@ -13,7 +16,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0) 
+	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
